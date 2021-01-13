278f849260a357483f241ab7d54fef13d72c6461
jude-lif <43428384+Jugue123@users.noreply.github.com>
2021-01-13 15:16:12 -0800

    fixed command stuff

--- src/main/java/org/team3128/compbot/autonomous/CmdAutoTrajectory.java
+++ src/main/java/org/team3128/compbot/autonomous/CmdAutoTrajectory.java
@@ -11,32 +11,48 @@ import org.team3128.compbot.subsystems.Constants;
 import org.team3128.common.utility.Log;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj.Timer;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
-public class CmdAutoTrajectory implements Command { 
+public class CmdAutoTrajectory implements Command {
+
+    private Set<Subsystem> requirements;
+
     public ArrayList<Pose2D> waypoints = new ArrayList<Pose2D>();
     public Trajectory trajectory;
 
     private double speed, acceleration, timeExpected, timeInitial, timeCurrent, timeoutMs;
     private FalconDrive drive;
-    
-    public CmdAutoTrajectory(FalconDrive drive, double speed, double acceleration, double timeoutMs, Pose2D... inputWaypoints) { 
+
+    public CmdAutoTrajectory(FalconDrive drive, double speed, double acceleration, double timeoutMs,
+            Pose2D... inputWaypoints) {
+
+        this.requirements = new HashSet<Subsystem>();
 
         this.speed = speed;
         this.acceleration = acceleration;
+
         this.drive = drive;
+        this.requirements.add(drive);
+
         this.timeoutMs = timeoutMs;
 
-        for(Pose2D ele : inputWaypoints) {
+        for (Pose2D ele : inputWaypoints) {
             waypoints.add(ele);
         }
-
-        addRequirements(drive);
     }
 
-    @Override protected void initialize() { 
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return requirements;
+    }
+
+    @Override
+    public void initialize() {
         trajectory = TrajectoryGenerator.generateTrajectory(waypoints, new ArrayList<TrajectoryConstraint>(), 0, 0,
                 speed * Constants.MechanismConstants.inchesToMeters, acceleration, false);
         drive.setAutoTrajectory(trajectory, false);
@@ -45,23 +61,18 @@ public class CmdAutoTrajectory implements Command {
         timeInitial = Timer.getFPGATimestamp();
     }
 
-    @Override protected synchronized boolean isFinished() {
+    @Override
+    public synchronized boolean isFinished() {
         timeCurrent = Timer.getFPGATimestamp();
         if ((timeCurrent - timeInitial) >= Math.min(timeExpected, timeoutMs)) {
-           return true;
+            return true;
         } else {
-           return false;
-       }
+            return false;
+        }
     }
 
-    @Override public void end() {
+    @Override
+    public void end(boolean interrupted) {
         drive.setWheelVelocity(new DriveSignal(0, 0));
     }
-
-   
-    public void interrupted() {
-        end();
-        Log.info("CmdAutoTrajectory", "Interrupted.");
-    }
-
 }
