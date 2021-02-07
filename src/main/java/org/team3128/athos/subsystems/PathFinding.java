958affa4c29d34cc736fb718689a09088cddf3ea
tyco333 <34726618+tyco333@users.noreply.github.com>
2021-02-07 11:44:19 -0800

    Reset Starting Position

--- src/main/java/org/team3128/athos/subsystems/PathFinding.java
+++ src/main/java/org/team3128/athos/subsystems/PathFinding.java
@@ -102,7 +102,7 @@ public class PathFinding {
                         .addConstraint(autoVoltageConstraint).setReversed(false);
         
         Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-                new Pose2d(1, 0, new Rotation2d(0)),
+                new Pose2d(0, 0, new Rotation2d(0)),
                 List.of(new Translation2d(3, 2)),
                 new Pose2d(5, 0, new Rotation2d(0)),
                 config);
